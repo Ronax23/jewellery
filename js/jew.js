5cedff74d857ae2b3ca5e8e1427714e7627cd3c7
Bhavya <48246206+Ronax23@users.noreply.github.com>
2025-06-13 19:47:30 +0530

    Add files via upload

--- js/jew.js
+++ js/jew.js
@@ -3,21 +3,23 @@
         clicker[0].addEventListener("click",()=>{
             clicker[0].classList.toggle("fa-xmark")
             menu[0].classList.toggle("active")
-            console.log(menu[0])
         })
 
         let products=document.querySelectorAll(".sec2 .block")
         let main=document.querySelector(".sec2 .main")
         let view_more=document.querySelector(".sec2 button")
         let check=0
-        view_more.addEventListener("click",()=>{
+     if(view_more)
+     {
+           view_more.addEventListener("click",()=>{
             if(check<2)
             {
                 check++
-                for(let i=0;i<=8;i++)
+                for(let i=0;i<8;i++)
                 {
                   main.innerHTML+=products[i].outerHTML
                 }
             }
             
         })
+     }

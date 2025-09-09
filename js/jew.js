bf403018ac3d820a3ff04ba4dc213c6003b4797d
Bhavya <48246206+Ronax23@users.noreply.github.com>
2025-09-09 18:11:25 +0530

    Add files via upload

--- js/jew.js
+++ js/jew.js
@@ -1,4 +1,18 @@
- let clicker=document.querySelectorAll("nav i")
+      async function fetch_dat() {
+      try{
+		  let result=await  fetch("https://fakestoreapi.com/products/category/jewelery")
+      return await result.json()
+	  }
+      catch(e)
+		  {
+			  console.log("error" +e)
+		  }
+      
+     }
+
+
+
+let clicker=document.querySelectorAll("nav i")
         let menu=document.querySelectorAll("nav .navs")
         clicker[0].addEventListener("click",()=>{
             clicker[0].classList.toggle("fa-xmark")
@@ -9,17 +23,40 @@
         let main=document.querySelector(".sec2 .main")
         let view_more=document.querySelector(".sec2 button")
         let check=0
+
+
+        let arr=fetch_dat()
      if(view_more)
      {
            view_more.addEventListener("click",()=>{
-            if(check<2)
+            if(check==0)
             {
                 check++
-                for(let i=0;i<8;i++)
-                {
-                  main.innerHTML+=products[i].outerHTML
-                }
+				view_more.innerText="Collapse"
+               arr.then(el=> el.forEach(element => { 
+                let product=document.createElement("section")
+                product.setAttribute("class","block fetched")
+               product.innerHTML= `    
+               
+                    <p>New</p>
+                    <img src="${element.image}" alt="">
+                    <section class="dat">
+                        <span>${element.title}</span>
+                        <span class="price">Price <span>&#8377;${element.price}</span></span>
+                    </section>`
+                 main.append(product)
+               
+                
+               }))
             }
+			   
+			   else{
+				   document.querySelectorAll(".fetched").forEach(em=> em.remove())
+				   view_more.innerText="View All Products"
+				   check--
+			   }
             
         })
      }
+
+
